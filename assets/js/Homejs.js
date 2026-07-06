f6885aac5520e989f248d0a4b4f2e77335a3e73e
oneandonlyshayanshahrokhi <shayanshahroukhipour@gmail.com>
2026-07-06 00:51:50 -0700

    0.14.3

--- assets/js/Homejs.js
+++ assets/js/Homejs.js
@@ -55,10 +55,15 @@ console.log(linkAnchor);
 const aboutmeAnchor=document.querySelector(".aboutme")
 const myskillsAnchor = document.querySelector(".Skills")
 const myservicesAnchor=document.querySelector(".Services")
+const myeducationAnchor = document.querySelector(".Education")
+const myJobAnchor =document.querySelector(".Job")
+
 //sections
 const myskillssection =document.querySelector("#skills")
 const aboutmesection = document.querySelector("#aboutMe")
 const myservicessection=document.querySelector("#services")
+const myeducation =document.querySelector("#education")
+const myjob = document.querySelector("#myJob")
 
  let start=0
  let viewportstartfromclick=0
@@ -75,6 +80,29 @@ aboutmeAnchor.addEventListener("click",(e)=>{
     requestAnimationFrame(animation);
 
 })
+
+myeducationAnchor.addEventListener("click",(e)=>{
+   e.preventDefault()
+   start=window.pageYOffset
+   viewportstartfromclick=myeducation.getBoundingClientRect().top + window.pageYOffset;
+   distance=viewportstartfromclick - start
+   startTime=null
+  
+    requestAnimationFrame(animation);
+
+})
+
+myJobAnchor.addEventListener("click",(e)=>{
+   e.preventDefault()
+   start=window.pageYOffset
+   viewportstartfromclick=myjob.getBoundingClientRect().top + window.pageYOffset;
+   distance=viewportstartfromclick - start
+   startTime=null
+  
+    requestAnimationFrame(animation);
+
+})
+
 
 myskillsAnchor.addEventListener("click",(e)=>{
    e.preventDefault()
@@ -101,16 +129,16 @@ myservicesAnchor.addEventListener("click",(e)=>{
 })
 
 function animation(currentTime) {
-    if (startTime === null) startTime = currentTime;
+    if (startTime === null) startTime = currentTime; //start the animation from bottom
 
-    const time = currentTime - startTime;
-    const progress = Math.min(time / duration, 1);
+    const time = currentTime - startTime; //how many of animation done
+    const progress = Math.min(time / duration, 1); //calculate between 0 to 1 
 
-    const ease = easeOutSine(progress);
+    const ease = easeOutSine(progress); //calling the easing function
 
-    window.scrollTo(0, start + distance * ease);
+    window.scrollTo(0, start + distance * ease); //the way scrolled from zero arriving to value of start plus distance of first scroll minus position of true ellement triple of easing function for smothing 
 
-    if (time < duration) {
+    if (time < duration) { //value of animation is gone < time of animation 
       requestAnimationFrame(animation);
     }
   }
