ac0507de7b788a27635b0ed8d8c96f0827b9b98b
oneandonlyshayanshahrokhi <shayanshahroukhipour@gmail.com>
2026-07-06 06:19:40 -0700

    0.14.4

--- assets/js/Homejs.js
+++ assets/js/Homejs.js
@@ -1,4 +1,3 @@
-let index = 0;
 
 const dotSlider = document.querySelectorAll(".dot");
 const img = document.querySelectorAll(".sliderImg");
@@ -49,107 +48,49 @@ window.addEventListener("scroll", () => {
 });
 
 
-const linkAnchor =document.querySelectorAll("a[href='#']")
+//smooth scroll
+const linkAnchor = document.querySelectorAll("a[href*='#']");
 console.log(linkAnchor);
-//menu Links
-const aboutmeAnchor=document.querySelector(".aboutme")
-const myskillsAnchor = document.querySelector(".Skills")
-const myservicesAnchor=document.querySelector(".Services")
-const myeducationAnchor = document.querySelector(".Education")
-const myJobAnchor =document.querySelector(".Job")
 
-//sections
-const myskillssection =document.querySelector("#skills")
-const aboutmesection = document.querySelector("#aboutMe")
-const myservicessection=document.querySelector("#services")
-const myeducation =document.querySelector("#education")
-const myjob = document.querySelector("#myJob")
+let start = 0;
+let viewportstartfromclick = 0;
+let distance = 0;
+let startTime = null;
+const duration = 1000;
 
- let start=0
- let viewportstartfromclick=0
- let distance = 0
- let startTime=null
-  const duration = 1000
-aboutmeAnchor.addEventListener("click",(e)=>{
-   e.preventDefault()
-   start=window.pageYOffset
-   viewportstartfromclick=aboutmesection.getBoundingClientRect().top + window.pageYOffset;
-   distance=viewportstartfromclick - start
-   startTime=null
-  
+linkAnchor.forEach((link, index) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = link.getAttribute("href");
+    const sec = document.querySelector(target);
+    console.log(target, sec);
+    console.log("clicked");
+    start = window.pageYOffset;
+    viewportstartfromclick =
+      sec.getBoundingClientRect().top + window.pageYOffset;
+    distance = viewportstartfromclick - start;
+    startTime = null;
+
     requestAnimationFrame(animation);
-
-})
-
-myeducationAnchor.addEventListener("click",(e)=>{
-   e.preventDefault()
-   start=window.pageYOffset
-   viewportstartfromclick=myeducation.getBoundingClientRect().top + window.pageYOffset;
-   distance=viewportstartfromclick - start
-   startTime=null
-  
-    requestAnimationFrame(animation);
-
-})
-
-myJobAnchor.addEventListener("click",(e)=>{
-   e.preventDefault()
-   start=window.pageYOffset
-   viewportstartfromclick=myjob.getBoundingClientRect().top + window.pageYOffset;
-   distance=viewportstartfromclick - start
-   startTime=null
-  
-    requestAnimationFrame(animation);
-
-})
-
-
-myskillsAnchor.addEventListener("click",(e)=>{
-   e.preventDefault()
-   start=window.pageYOffset
-   viewportstartfromclick=myskillssection.getBoundingClientRect().top + window.pageYOffset;
-   distance=viewportstartfromclick - start
-      startTime=null
-
-  
-    requestAnimationFrame(animation);
-
-})
-
-myservicesAnchor.addEventListener("click",(e)=>{
-   e.preventDefault()
-   start=window.pageYOffset
-   viewportstartfromclick=myservicessection.getBoundingClientRect().top + window.pageYOffset;
-   distance=viewportstartfromclick - start
-    startTime=null
-
-  
-    requestAnimationFrame(animation);
-
-})
+  });
+});
 
 function animation(currentTime) {
-    if (startTime === null) startTime = currentTime; //start the animation from bottom
+  if (startTime === null) startTime = currentTime; //start the animation from bottom
 
-    const time = currentTime - startTime; //how many of animation done
-    const progress = Math.min(time / duration, 1); //calculate between 0 to 1 
+  const time = currentTime - startTime; //how many of animation done
+  const progress = Math.min(time / duration, 1); //calculate between 0 to 1
 
-    const ease = easeOutSine(progress); //calling the easing function
+  const ease = easeOutSine(progress); //calling the easing function
 
-    window.scrollTo(0, start + distance * ease); //the way scrolled from zero arriving to value of start plus distance of first scroll minus position of true ellement triple of easing function for smothing 
+  window.scrollTo(0, start + distance * ease); //the way scrolled from zero arriving to value of start plus distance of first scroll minus position of true ellement triple of easing function for smothing
 
-    if (time < duration) { //value of animation is gone < time of animation 
-      requestAnimationFrame(animation);
-    }
+  if (time < duration) {
+    //value of animation is gone < time of animation
+    requestAnimationFrame(animation);
   }
+}
 
 function easeOutSine(x) {
   return Math.sin((x * Math.PI) / 2);
 }
-
-
-
-
-
-
-
